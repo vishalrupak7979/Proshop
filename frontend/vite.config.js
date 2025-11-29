b744be008cd3bdbb99330fc7ea7e184320e3e837
vishalrupak7979 <vishalrupakvr@gmail.com>
2025-11-29 13:03:43 +0530

    changes rediection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
-    port: 3000,
+    historyApiFallback: true
   }
 })
